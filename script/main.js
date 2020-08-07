9cc46e5b440c48f5e9d0063f3940ed2d77e2367a
sahal alzubair <inzilagiway@gmail.com>
2020-08-07 15:27:23 +0300

    navbar works with phones

--- script/main.js
+++ script/main.js
@@ -1,6 +1,7 @@
 // hamburger menu
 
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+/*
 function mobileNav() {
     var x = document.getElementById("main.nav");
     if (x.style.display === "block") {
